d6fae8349ddb8c6141f170bde756177197b12d09
Hojiakbar <hojiakbarnasriddinov@Hojiakbars-MacBook-Air.local>
2024-01-11 19:46:35 +0500

    register

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import OneTimePassword
+from .utils import send_code_to_user
+from .serializers import UserRegisterSerializer
 
-# Create your views here.
+
+class RegisterUserView(GenericAPIView):
+    serializer_class = UserRegisterSerializer
+
+    def post(self, request):
+        user_data = request.data
+        serializer = self.serializer_class(data=user_data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            user = serializer.data
+            send_code_to_user(user['email'])
+            first_name = user['first_name']
+            return Response({
+                'data': user,
+                'message': f'{first_name} passcode has be sent to verify your email',
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class VerifyUserEmail(GenericAPIView):
+    def post(self, request):
+        otp_code = request.data.get('otp')
+        try:
+            user_code_obj = OneTimePassword.objects.get(code=otp_code)
+            user = user_code_obj.user
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+                return Response({
+                    'message': 'account email verified successfully'
+                }, status=status.HTTP_200_OK)
+            return Response({'message': 'passcode is invalid user is already verified'}, status=status.HTTP_204_NO_CONTENT)
+        except OneTimePassword.DoesNotExist:
+            return Response({'message': 'passcode not provided'}, status=status.HTTP_404_NOT_FOUND)
--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restframework',
+    'rest_framework',
     'corsheaders',
     'accounts',
     'social_accounts',
@@ -78,6 +78,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL='accounts.User'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -119,3 +121,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587 
+DEFAULT_FROM_EMAIL='mySite@gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
